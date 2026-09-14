9fa5a37138803660aae5bbe81ef2412a191fd996
Francesco Tenerelli <149258480+NullMC@users.noreply.github.com>
2026-09-14 22:13:10 +0200

    style: apply sponsor and partner layout refinements

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./styles/hero-scroller.css";
 import "./styles/app-layout.css";
 import "./styles/chrome-responsive.css";
 import "./styles/sponsor-neutral.css";
+import "./styles/partner-tweaks.css";
 import Home from "./pages/Home";
 import Competitions from "./pages/Competitions";
 import Standings from "./pages/Standings";
