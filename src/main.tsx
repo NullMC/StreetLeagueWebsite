dc2ba5d34455d42c48d68a9ced913c71ffaae2ae
Francesco Tenerelli <149258480+NullMC@users.noreply.github.com>
2026-09-16 13:56:38 +0200

    feat(admin): expose POTM from back office navigation

--- src/main.tsx
+++ src/main.tsx
@@ -27,7 +27,7 @@ import Matches from "./pages/Matches";
 import MatchDetail from "./pages/MatchDetail";
 import Partners from "./pages/Partners";
 import Collaborate from "./pages/Collaborate";
-import Admin from "./pages/Admin";
+import AdminWithPotmNav from "./pages/AdminWithPotmNav";
 import AdminPotm from "./pages/AdminPotm";
 import NotFound from "./pages/NotFound";
 import { AppLayout } from "./components/AppLayout";
@@ -49,7 +49,7 @@ function App() {
         <Route path="/partite/:matchId" element={<MatchDetail />} />
         <Route path="/partner" element={<Partners />} />
         <Route path="/collabora" element={<Collaborate />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin" element={<AdminWithPotmNav />} />
         <Route path="/admin/potm" element={<AdminPotm />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
